cff6aaf05e9a2299a9a55d78424e3f147e776f37
Dan Costinett <dan@cozz.net>
2014-10-11 13:28:33 -0700

    inital commit

---
+++ app/src/main/java/net/cozz/danco/homework1/MyActivity.java
@@ -0,0 +1,72 @@
+package net.cozz.danco.homework1;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import static android.view.View.OnClickListener;
+
+
+public class MyActivity extends Activity implements OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+        View view1 = findViewById(R.id.view1);
+        view1.setOnClickListener(this);
+        View view2 = findViewById(R.id.view2);
+        view2.setOnClickListener(this);
+        View view3 = findViewById(R.id.view3);
+        view3.setOnClickListener(this);
+        View view4 = findViewById(R.id.view4);
+        view4.setOnClickListener(this);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        TextView tv = (TextView)findViewById(R.id.textView1);
+
+        switch (v.getId()) {
+            case R.id.view1:
+                v.setSelected(true);
+                tv.setText("Red");
+                break;
+            case R.id.view2:
+                v.setSelected(true);
+                tv.setText("Green");
+                break;
+            case R.id.view3:
+                v.setSelected(true);
+                tv.setText("Blue");
+                break;
+            case R.id.view4:
+                v.setSelected(true);
+                tv.setText("Black");
+                break;
+        }
+    }
+}
